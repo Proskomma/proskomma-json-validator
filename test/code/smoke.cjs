1e08ce0414f917ea6a5c77f351fdf103241caf70
Mark Howe <mvahowe@gmail.com>
2022-05-23 20:01:52 +0200

    Passing validator test

--- test/code/smoke.cjs
+++ test/code/smoke.cjs
@@ -9,10 +9,12 @@ test(
     `Hello (${testGroup})`,
     async function (t) {
         try {
-            t.plan(2);
+            t.plan(3);
             const validator = new ProskommaJsonValidator();
             t.throws(()=> validator.validate('banana', {}), 'Unknown');
             t.notOk(validator.validate('perf', {}));
+            const goodPerf = fse.readJsonSync(path.resolve(path.join('test', 'test_data', 'fra_lsg_jon.json')));
+            t.ok(validator.validate('perf', goodPerf));
         } catch (err) {
             console.log(err);
         }

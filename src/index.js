a8d79302da7ca5e740c7ea14ac5ee11386b8f299
Mark Howe <mvahowe@gmail.com>
2022-05-24 11:02:03 +0200

    Nested schemas to validate docSets, docSet, document and sequence

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import Ajv from 'ajv';
-import perfSchema from './perf_schema';
+import docSetsSchema from './schema/docSetsSchema.json';
+import docSetSchema from './schema/docSetSchema.json';
+import documentSchema from './schema/documentSchema.json';
+import sequenceSchema from './schema/sequenceSchema.json';
 
 class ProskommaJsonValidator {
 
     constructor() {
         this.schema = {};
-        for (const [key, schema] of [['perf', perfSchema]]) {
+        for (const [key, schema] of [
+            [
+                'docSetsPerf',
+                new Ajv()
+                    .addSchema(sequenceSchema)
+                    .addSchema(documentSchema)
+                    .addSchema(docSetSchema)
+                    .compile(docSetsSchema)
+            ],
+            [
+                'docSetPerf',
+                new Ajv()
+                    .addSchema(sequenceSchema)
+                    .addSchema(documentSchema)
+                    .compile(docSetSchema)
+            ],
+            [
+                'documentPerf',
+                new Ajv()
+                    .addSchema(sequenceSchema)
+                    .compile(documentSchema)
+            ],
+            [
+                'sequencePerf',
+                new Ajv()
+                    .compile(sequenceSchema)
+            ],
+        ]) {
             this.schema[key] = schema;
         }
     }
@@ -14,7 +44,7 @@ class ProskommaJsonValidator {
         if (!(schemaKey)) {
             throw new Error(`Unknown schema key '${schemaKey}'`)
         }
-        const validator = new Ajv().compile(this.schema[schemaKey]);
+        const validator = this.schema[schemaKey];
         return {
             schemaKey,
             isValid: validator(data),
